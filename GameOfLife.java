ae98bf445a8f95a8930ed0564c28a94ea9f0576c
Esther Livingstone <esther.livingstone@post.runi.ac.il>
2024-01-21 15:03:24 +0200

    Update GameOfLife.java

--- GameOfLife.java
+++ GameOfLife.java
@@ -190,12 +190,12 @@ private static int countNeighbors(int[][] board, int row, int col) {
 	// Prints the board. Alive and dead cells are printed as 1 and 0, respectively.
     public static void print(int[][] arr) {
         for (int i = 1; i < arr.length; i++) {
-            for (int j = 1; j < arr[0].length; j++) {
-                System.out.print("%3s" , arr[i][j]);
-            }
-            System.out.println();
-        }
-    }
+			for (int j = 1; j < arr[0].length; j++) {
+				System.out.printf("%3d", arr[i][j]);  
+			}
+			System.out.println();
+		}
+	}
 		
     // Displays the board. Living and dead cells are represented by black and white squares, respectively.
     // We use a fixed-size canvas of 900 pixels by 900 pixels for displaying game boards of different sizes.
